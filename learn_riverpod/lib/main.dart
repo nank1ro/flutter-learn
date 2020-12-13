75a56887ac1373fbf969519981c4ddd2ed97c2e0
Alexandru Mariuti <alexandrumariuti@protonmail.com>
2020-12-13 16:44:21 +0100

    add stateprovider example

--- learn_riverpod/lib/main.dart
+++ learn_riverpod/lib/main.dart
@@ -10,6 +10,11 @@ final numberProvider = Provider<int>((ref) {
   return 10;
 });
 
+// Basic number state provider.
+final numberStateProvider = StateProvider<int>((ref) {
+  return 20;
+});
+
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
@@ -28,11 +33,23 @@ class MyHomePage extends ConsumerWidget {
   @override
   Widget build(BuildContext context, ScopedReader watch) {
     final number = watch(numberProvider);
+    final numberState = watch(numberStateProvider).state;
 
     return Scaffold(
       body: Center(
-        child: Text('$number'),
+        child: Text('$numberState'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => increment(context),
+        child: Icon(
+          Icons.plus_one,
+        ),
       ),
     );
+  }
+
+  /// Used to increment the value of the number and to see changes.
+  void increment(BuildContext context) {
+    context.read(numberStateProvider).state++;
   }
 }
