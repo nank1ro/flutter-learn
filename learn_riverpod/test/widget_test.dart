987b755509bfb9cbfbec542ca5b3c1eb483551ab
Alexandru Mariuti <alexandrumariuti@protonmail.com>
2020-12-13 16:24:43 +0100

    rename package

--- learn_riverpod/test/widget_test.dart
+++ learn_riverpod/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_riverpod/main.dart';
+import 'package:learn_riverpod/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
